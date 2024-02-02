adddeeee5aa5c6aad5ed2a0eea0707b59c379b89
jiajianhui <jia15176168273@icloud.com>
2024-02-02 12:23:43 +0800

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by 贾建辉 on 2024/2/2.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
