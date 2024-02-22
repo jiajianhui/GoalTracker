81be26f57e091553a67c6b59bfe9e40be2efce54
jiajianhui <jia15176168273@icloud.com>
2024-02-22 15:10:28 +0800

    将GoalRowView中的视图组件化

--- GoalTracker/Views/GoalRowView.swift
+++ GoalTracker/Views/GoalRowView.swift
@@ -27,29 +27,8 @@ struct GoalRowView: View {
     var body: some View {
         VStack(spacing: 20) {
             HStack {
-                VStack(alignment: .leading, spacing: 8) {
-                    Text(vm.goal.title)
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .lineLimit(showDetail ? 100 : 1)
-                    Text(displayDate(vm.goal.date))
-                        .font(.system(size: 12, weight: .medium, design: .rounded))
-                        .opacity(0.6)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background {
-                    Color.white.opacity(0.0001)
-                }
-                
-                Text(vm.goal.schedule == 10 ? "已完成" : "进行中")
-                    .font(.system(size: 12, weight: .bold, design: .rounded))
-                    .foregroundStyle(vm.goal.schedule == 10 ? .green : .purple)
-                    .padding(10)
-                    .background {
-                        vm.goal.schedule == 10 ? Color.green.opacity(0.08) : Color.purple.opacity(0.08)
-                    }
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                
+                titleAndDate
+                state
             }
             .onTapGesture {
                 UIImpactFeedbackGenerator.impact(style: .light)
@@ -70,65 +49,13 @@ struct GoalRowView: View {
             
             if showDetail {
                 VStack(spacing: 19) {
-                    
                     LineView()
-                    HStack {
-                        Text("当前进度")
-                        Spacer()
-                        Gauge(
-                            value: Double(vm.goal.schedule) / 10,  //将schedule转为小数，以供Gauge使用——计算属性
-                            label: {
-                                Text("进度")
-                            },
-                            currentValueLabel: {
-                                Text("\(vm.goal.schedule * 10)%") //将schedule转为百分比——计算属性
-                                    .font(.system(size: 16, weight: .medium))
-                            }
-                        )
-                        .gaugeStyle(.accessoryCircularCapacity)
-                        .tint(vm.goal.schedule == 10 ? .green : .purple )
-                    }
+                    gauge
                     LineView()
                     Stepper("更新任务进度", value: $vm.goal.schedule, in: 0...10, step: 1)
                     LineView()
-                    HStack {
-                        Button {
-                            withAnimation(.easeOut) {
-                                selectedGoal = vm.goal
-                                showDetail.toggle()
-                            }
-                        } label: {
-                            Text("编辑")
-                                .foregroundStyle(Color.purple)
-                                .frame(width: 80, height: 60)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 16)
-                                        .fill(Color.gray.opacity(0.1))
-                                }
-                        }
-
-                        Spacer()
-                        Button {
-                            withAnimation(.easeOut) {
-                                vm.save()
-                                showDetail.toggle()
-                            }
-                        } label: {
-                            Text("保存")
-                                .fontWeight(.bold)
-                                .foregroundStyle(Color.white)
-                                .frame(maxWidth: .infinity)
-                                .frame(height: 60)
-                                .background {
-                                    RoundedRectangle(cornerRadius: 16)
-                                        .fill(Color.purple)
-                                }
-                        }
-
-                    }
-                    .padding(.top, 8)
+                    btns
                 }
-                
             }
         }
         .padding(24)
@@ -144,7 +71,7 @@ struct GoalRowView: View {
         let formatter = DateFormatter()
         
         formatter.locale = Locale(identifier: "zh_Hans")
-        formatter.setLocalizedDateFormatFromTemplate("HH:mm MM-dd")
+        formatter.setLocalizedDateFormatFromTemplate("MM-dd")
         
         return formatter
     }()
@@ -204,6 +131,99 @@ extension GoalRowView {
             }
         }
     }
+    
+    //MARK: 目标标题、时间
+    private var titleAndDate: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(vm.goal.title)
+                .font(.title2)
+                .fontWeight(.bold)
+                .lineLimit(showDetail ? 100 : 1)
+            Text(displayDate(vm.goal.date))
+                .font(.system(size: 12, weight: .medium, design: .rounded))
+                .opacity(0.6)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background {
+            Color.white.opacity(0.0001)
+        }
+    }
+    
+    //MARK: 目标状态
+    private var state: some View {
+        Text(vm.goal.schedule == 10 ? "已完成" : "进行中")
+            .font(.system(size: 12, weight: .bold, design: .rounded))
+            .foregroundStyle(vm.goal.schedule == 10 ? .green : .purple)
+            .padding(10)
+            .background {
+                vm.goal.schedule == 10 ? Color.green.opacity(0.08) : Color.purple.opacity(0.08)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+    
+    //MARK: - 详情部分
+    private var gauge: some View {
+        HStack {
+            Text("当前进度")
+            Spacer()
+            Gauge(
+                value: Double(vm.goal.schedule) / 10,  //将schedule转为小数，以供Gauge使用——计算属性
+                label: {
+                    Text("进度")
+                },
+                currentValueLabel: {
+                    Text("\(vm.goal.schedule * 10)%") //将schedule转为百分比——计算属性
+                        .font(.system(size: 16, weight: .medium))
+                }
+            )
+            .gaugeStyle(.accessoryCircularCapacity)
+            .tint(vm.goal.schedule == 10 ? .green : .purple )
+        }
+    }
+    
+    private var btns: some View {
+        HStack {
+            
+            Button {
+                withAnimation(.easeOut) {
+                    selectedGoal = vm.goal
+                    showDetail.toggle()
+                }
+            } label: {
+                Text("编辑")
+                    .foregroundStyle(Color.purple)
+                    .frame(width: 80, height: 60)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.gray.opacity(0.1))
+                    }
+            }
+
+            Spacer()
+            
+            Button {
+                withAnimation(.easeOut) {
+                    vm.save()
+                    showDetail.toggle()
+                }
+            } label: {
+                Text("保存")
+                    .fontWeight(.bold)
+                    .foregroundStyle(Color.white)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 60)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.purple)
+                    }
+            }
+
+        }
+        .padding(.top, 8)
+    }
+    
+    
+    
 }
 
 //#Preview {
